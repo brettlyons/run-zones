111403bf2acd3104e95fa3dfa6e2d87b7bd23415
Brett Lyons <blyons@students.naropa.edu>
2016-03-03 20:50:10 -0700

    Hopefully this added bootstrap.

--- src/clj/run_zones/handler.clj
+++ src/clj/run_zones/handler.clj
@@ -18,6 +18,7 @@
      [:meta {:charset "utf-8"}]
      [:meta {:name "viewport"
              :content "width=device-width, initial-scale=1"}]
+     (include-css "css/bootstrap.min.css" "css/bootstrap.css")
      (include-css (if (env :dev) "css/site.css" "css/site.min.css"))]
     [:body
      mount-target
