150cbbc0768294fca94ab093a0144c8c6d0a6d33
Brett Lyons <blyons@students.naropa.edu>
2016-03-11 11:57:14 -0700

    Fiddling with things, removed unnecessary comments

--- src/cljs/run_zones/core.cljs
+++ src/cljs/run_zones/core.cljs
@@ -4,13 +4,6 @@
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]))
 
-;; -------------------------
-;; Views
-;; (defn bootstrap-head []
-;;   [:div {:id "bs"}
-;;    [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" :integrity "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" :crossorigin "anonymous"}]
-;;    [:script {:src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" :integrity "sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" :crossorigin "anonymous"}]])
-
 (defn home-page []
   [:div [:h2 "Welcome to run-zones"]
    [:button.btn.btn-success {:on-click (fn [e] (println "Jamica!"))} "Button!"]
